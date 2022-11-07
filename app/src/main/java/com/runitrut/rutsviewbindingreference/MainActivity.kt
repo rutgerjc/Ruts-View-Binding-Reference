0e56b8c987307019b528be9128c27856f0e838cd
rutgerjc <colonrutger@gmail.com>
2022-11-07 08:56:34 -0500

    Updated dependencies and comment's

--- app/src/main/java/com/runitrut/rutsviewbindingreference/MainActivity.kt
+++ app/src/main/java/com/runitrut/rutsviewbindingreference/MainActivity.kt
@@ -10,19 +10,21 @@ class MainActivity : AppCompatActivity() {
     // is THE CONVENTION */
     /* Build binding here like so : EXAMPLE BELOEW
     private lateinit var binding: ActivityMainBinding
+    /* ^^^^^^^^^^^ SECOND STEP ^^^^^^^^^^^^^^^^^^^^^^^*/
     */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /* OR building the binding VAR here, like so : EXAMPLE BELOW */
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        /* ^^^^^^^^^^^ part of SECOND STEP ^^^^^^^^^^^^^^^^^^^^^^^*/
+        /* ^^^^^^^^^^^ CONDITIONAL SECOND STEP ^^^^^^^^^^^^^^^^^^^^^^^*/
 
         //TODO THIRD step, setContentView() and add "binding.root"setContentView(binding.root)
         setContentView(binding.root)
         /* ^^^^^^^^ THIRD STEP ^^^^^^^^^^^^^*/
 
-        //TODO FOURTH, and final step. Now that viewbinding is ENABLE, AND you've created a binding variable called: "binding"
+        //TODO FOURTH, and final step. Now that viewbinding is ENABLE, AND you've created a binding variable called: "binding" -
+        // that extends the current class youre trying to use V.B. on
         // The CURRENT Activity is now ASSOCIATED WITH ITS CORRESPONDING XML FILE.
         // Call bind.W/E viewID you need access to
         //  =^)    */
@@ -30,5 +32,6 @@ class MainActivity : AppCompatActivity() {
             binding.textView1.text = "Viewbinding Replaces findviewbyID"
             binding.textView2.text = " OH that's cool and efficeint!"
         }
+        /* ^^^^^^^^^^^ FOURTH STEP ^^^^^^^^^^^^^^^^^^^^^^^*/
     }
 }
